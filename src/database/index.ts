250084a9ae71cb63dba3c6d0b8e0f01ab88aa5c0
Cleiton <cleiton.biou@gmail.com>
2022-06-03 21:11:56 -0300

    remove databse url
    Signed-off-by: Cleiton <cleiton.biou@gmail.com>

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb+srv://admin:Iow8tF6wmhvxT9I1@cluster0.k4x76.mongodb.net/sandney?retryWrites=true&w=majority').then(() => {
+mongoose.connect('').then(() => {
   console.log('MongoDB connected')
 }).catch(err => {
   console.log(err)
